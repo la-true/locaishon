cd400018cc80b7cf567e9a6622d5049098a8c5f7
Fabiola Ampuero <fabi.ampuero.chaclco@gmail.com>
2017-02-16 14:50:00 -0500

    oooooosiiibaby

--- js/app.js
+++ js/app.js
@@ -28,17 +28,33 @@ function initMap() {
         }
     );
     
-    var latLongPazPeru = {
-        lat: -16.457736, 
-        lng: -71.530440
-    };
+    var latLongPazPeru = {lat: -16.457736, lng: -71.530440};
+    var latDos = {lat: -16.456736, lng: -71.530140};
+    var latTres = {lat: -16.453736, lng: -71.530640};
+    var latCuatro = {lat: -16.451736, lng: -71.530940};
+    var latCinco = {lat: -16.459736, lng: -71.530340};
     
     var pazPeru = new google.maps.Marker({
     position: latLongPazPeru,
     map: miMapa,
-    label: "m",
+    label: "l",
+    icon: 'image/carrin.png',
     title: 'Aqui estamos!!!'
     });
+    
+    
+    new google.maps.Marker({position: latDos, map: miMapa,
+    label: "l",
+    icon: 'image/carrin.png'});
+    new google.maps.Marker({position: latTres, map: miMapa,
+    label: "l",
+    icon: 'image/carrin.png'});
+    new google.maps.Marker({position: latCuatro, map: miMapa,
+    label: "l",
+    icon: 'image/carrin.png'});
+    new google.maps.Marker({position: latCinco, map: miMapa,
+    label: "l",
+    icon: 'image/carrin.png'});
     
     var infowindow = new google.maps.InfoWindow({
     content: contentString
